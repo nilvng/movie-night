21a7439930eb683769a98a266565598bc3fbcc25
Nil Nguyen <nil@blinq.me>
2023-06-17 18:52:07 +1000

    🚸 Add navigation animation between main and detail view

--- app/src/main/java/com/nillin/movienight/MainActivity.kt
+++ app/src/main/java/com/nillin/movienight/MainActivity.kt
@@ -2,12 +2,17 @@ package com.nillin.movienight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nillin.movienight.ui.main.MainFragment
+import androidx.activity.addCallback
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onBackPressedDispatcher.addCallback(this) {
+            findNavController(R.id.nav_host_fragment).popBackStack()
+        }
     }
 }
