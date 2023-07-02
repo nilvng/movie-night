a1d21cd5a70d45716c22793a0627b48bcc201dfd
Nil Nguyen <nil@blinq.me>
2023-07-02 11:54:53 +1000

    💄 (search): Add search button, activity

---
+++ app/src/main/java/com/nillin/movienight/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.nillin.movienight.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nillin.movienight.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
