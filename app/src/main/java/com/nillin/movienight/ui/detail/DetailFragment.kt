a75a7efb79ba6c5d26b71f5c8bc5cb9bdb719f3d
Nil Nguyen <nil@blinq.me>
2023-07-09 19:24:31 +1000

    ✨ Load data into Detail screen

--- app/src/main/java/com/nillin/movienight/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/nillin/movienight/ui/detail/DetailFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
 import com.nillin.movienight.databinding.FragmentDetailBinding
+import com.nillin.movienight.local.movie.asState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     companion object {
         const val ARG_POSITION = "position"
     }
+
     private val binding: FragmentDetailBinding get() = _binding!!
     private var _binding: FragmentDetailBinding? = null
-    private lateinit var viewModel: DetailViewModel
+    private val viewModel: DetailViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,19 +39,25 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = DetailViewModel(requireActivity().application)
-        viewModel.movie.observe(viewLifecycleOwner) {
-            binding.tvSynopsisDetail.text = it.synopsis
-            binding.tvAuthor.text = it.creator
-            binding.tvTitle.text = it.title
-            binding.tvYear.text = it.year.toString()
-            Glide.with(binding.root.context)
-                .load(it.cover)
-                .fitCenter()
-                .into(binding.ivCoverPhoto)
-        }
-        arguments?.getInt(ARG_POSITION)?.let {
-            viewModel.fetch(it)
+        arguments?.getInt(ARG_POSITION)?.let { id ->
+            lifecycleScope.launch {
+                viewLifecycleOwner.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+                    viewModel.movieRepo.getById(id)
+                        .stateIn(this)
+                        .filterNotNull()
+                        .map { it.asState() }
+                        .collect {
+                            binding.tvSynopsisDetail.text = it.synopsis
+                            binding.tvAuthor.text = it.creator
+                            binding.tvTitle.text = it.title
+                            binding.tvYear.text = it.year.toString()
+                            Glide.with(binding.root.context)
+                                .load(it.cover)
+                                .fitCenter()
+                                .into(binding.ivCoverPhoto)
+                        }
+                }
+            }
         }
     }
 
--- app/src/main/java/com/nillin/movienight/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/nillin/movienight/ui/detail/DetailViewModel.kt
@@ -1,23 +1,29 @@
 package com.nillin.movienight.ui.detail
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.nillin.movienight.common.ContextViewModel
-import com.nillin.movienight.state.Movie
-import com.nillin.movienight.ui.main.dummy_data
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.nillin.movienight.local.movie.MovieRepo
+import com.nillin.movienight.local.movie.asState
+import com.nillin.movienight.state.MovieUI
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel
 @Inject constructor(
-    application: Application
-) : ContextViewModel(application) {
-    val movie: LiveData<Movie> get() = _movie
-    private val _movie: MutableLiveData<Movie> = MutableLiveData()
+    val movieRepo: MovieRepo
+) : ViewModel(){
+    val movieUI: LiveData<MovieUI?> get() = _movieUI
+    private val _movieUI: MutableLiveData<MovieUI> = MutableLiveData()
 
     fun fetch(index: Int) {
-        _movie.value = dummy_data[index]
+        viewModelScope.launch {
+            _movieUI.value = movieRepo.getById(index).asLiveData(coroutineContext).value?.asState()
+
+        }
     }
 }
